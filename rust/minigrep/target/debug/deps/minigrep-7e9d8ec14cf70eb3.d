5868c3eab0b6098ef75dfa00846bd86b8e42640d
revazashvili <levanrevazashvili@gmail.com>
2022-08-18 23:31:35 +0400

    gitignore

--- rust/minigrep/target/debug/deps/minigrep-7e9d8ec14cf70eb3.d
+++
@@ -1,5 +0,0 @@
-/home/revazashvili/Projects/samples/rust/minigrep/target/debug/deps/minigrep-7e9d8ec14cf70eb3: src/main.rs
-
-/home/revazashvili/Projects/samples/rust/minigrep/target/debug/deps/minigrep-7e9d8ec14cf70eb3.d: src/main.rs
-
-src/main.rs:
--- rust/minigrep/target/debug/minigrep.d
+++
@@ -1,1 +0,0 @@
-/home/revazashvili/Projects/samples/rust/minigrep/target/debug/minigrep: /home/revazashvili/Projects/samples/rust/minigrep/src/lib.rs /home/revazashvili/Projects/samples/rust/minigrep/src/main.rs
