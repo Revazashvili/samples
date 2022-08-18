fb4dc1f9ddcd541654c463c49491739f510512d2
revazashvili <levanrevazashvili@gmail.com>
2022-08-18 23:48:14 +0400

    add search function with test

--- rust/minigrep/src/lib.rs
+++ rust/minigrep/src/lib.rs
@@ -3,8 +3,9 @@ use std::error::Error;
 
 pub fn run(config: Config) -> Result<(),Box<dyn Error>>{
     let contents = fs::read_to_string(config.filename)?;
-    println!("With text: \n {}",contents);
-    println!("Contains: {}",contents.contains(&config.query));
+    for line in search(&config.query, &contents) {
+        println!("{}",line)
+    }
     Ok(())
 }
 
@@ -19,5 +20,30 @@ impl Config {
             return Err("not enough arguments");
         }
         Ok(Config { query:args[1].clone(), filename:args[2].clone() })
+    }
+}
+
+pub fn search<'a>(query: &str,contents: &'a str) -> Vec<&'a str>{
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line)
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    #[test]
+    fn one_result(){
+        let query = "duct";
+        let contents = "\n
+Rust:\n
+safe,fast,productive.\n
+Pick three.";
+        assert_eq!(vec!["safe,fast,productive."],search(query,contents))
     }
 }
