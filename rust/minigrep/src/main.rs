8c73958972ef891d025ed7d7ff09a43f532b73c1
revazashvili <levanrevazashvili@gmail.com>
2022-08-18 23:06:17 +0400

    refactor reading args

--- rust/minigrep/src/main.rs
+++ rust/minigrep/src/main.rs
@@ -1,22 +1,37 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let config = Config::new(env::args().collect())
+        .unwrap_or_else(|err| {
+            println!("Problem parsing arguments: {}",err);
+            process::exit(1)
+        });
 
-    let query = &args[1];
+    println!("Searching for {}",config.query);
+    println!("In file {}",config.filename);
 
-    let filename = &args[2];
-
-    println!("Searching for {}",query);
-    println!("In file {}",filename);
-
-    let contents = fs::read_to_string(filename)
+    let contents = fs::read_to_string(config.filename)
         .expect("something went wrong while reading file");
 
     println!("With text: \n {}",contents);
 
-    let contains = contents.contains(query);
+    let contains = contents.contains(&config.query);
     println!("Contains: {}",contains);
 
+}
+
+struct Config {
+    query: String,
+    filename: String
+}
+
+impl Config {
+    fn new(args: Vec<String>) -> Result<Config,&'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        Ok(Config { query:args[1].clone(), filename:args[2].clone() })
+    }
 }
