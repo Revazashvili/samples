a3026977604d687d8b6f92ddb954aee01ba58292
revazashvili <levanrevazashvili@gmail.com>
2022-08-18 22:44:22 +0400

    add simple search in file with command line args

--- rust/minigrep/src/main.rs
+++ rust/minigrep/src/main.rs
@@ -1,3 +1,22 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+
+    let filename = &args[2];
+
+    println!("Searching for {}",query);
+    println!("In file {}",filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("something went wrong while reading file");
+
+    println!("With text: \n {}",contents);
+
+    let contains = contents.contains(query);
+    println!("Contains: {}",contains);
+
 }
