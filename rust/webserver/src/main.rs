7a765e3431cd8c5cfe5ba1fa471aa3b6d566bb5d
revazashvili <levanrevazashvili@gmail.com>
2022-08-20 01:22:52 +0400

    return html content

--- rust/webserver/src/main.rs
+++ rust/webserver/src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::net::{TcpListener, TcpStream};
 use std::io::{Read, Write};
 
@@ -19,9 +20,14 @@ fn handle_connection(mut stream: TcpStream){
     let mut buffer = [0;1024];
 
     stream.read(&mut buffer).unwrap();
-    println!("Request: {}",String::from_utf8_lossy(&buffer));
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let contents = fs::read_to_string("index.html").unwrap();
+
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
