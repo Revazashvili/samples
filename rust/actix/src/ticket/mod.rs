f167d273ba8595b159a7961e9441187e70264715
revazashvili <levanrevazashvili@gmail.com>
2022-08-20 18:27:56 +0400

    make custom struct  for guarding

--- rust/actix/src/ticket/mod.rs
+++ rust/actix/src/ticket/mod.rs
@@ -1,13 +1,27 @@
 pub mod models;
 pub mod handlers;
 
-use actix_web::{web,guard};
+use actix_web::{web,guard::{Guard, GuardContext}};
 use handlers::*;
+
+struct ApiKeyHeader;
+
+impl Guard for ApiKeyHeader {
+    fn check(&self, req: &GuardContext) -> bool {
+        let api_key_header = req.head()
+            .headers()
+            .get("x-api-key");
+        match api_key_header{
+            Some(api_key) => api_key == "actix",
+            None => false,
+        }
+    }
+}
 
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/api")
-        .guard(guard::Header("api-key", "actix"))
+        .guard(ApiKeyHeader)
         .service(post_ticket)
         .service(get_tickets)
         .service(get_ticket)
