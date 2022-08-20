4db460b494aa8959d51aacdbe1ae41a96b31cecd
revazashvili <levanrevazashvili@gmail.com>
2022-08-20 14:38:12 +0400

    add post endpoint

--- rust/actix/src/main.rs
+++ rust/actix/src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, post, put, delete, web, App, HttpRequest, HttpResponse, HttpServer, Responder, ResponseError};
+use actix_web::{get, post, put, delete, web, App, HttpRequest, HttpResponse, HttpServer, Responder, ResponseError, CustomizeResponder};
 use actix_web::http::header::ContentType;
 use actix_web::http::StatusCode;
 use actix_web::body::BoxBody;
@@ -8,23 +8,52 @@ use serde::{Serialize, Deserialize};
 use std::fmt::Display;
 use std::sync::Mutex;
 
-fn main() {
-    println!("Hello, world!");
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("server starting...");
+    let app_state = web::Data::new(AppState{
+        tickets: Mutex::new(vec![
+            Ticket::new(1,String::from("Jane Doe")),
+            Ticket::new(1,String::from("Patrick Star"))
+        ])
+    });
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(app_state.clone())
+            .service(post_ticket)
+    })
+    .bind(("127.0.0.1", 7878))?
+    .run()
+    .await
+}
+
+#[post("/tickets")]
+async fn post_ticket(req: HttpRequest,ticket: web::Json<Ticket>,data: web::Data<AppState>) -> impl Responder{
+    println!("{:?}",ticket);
+    let new_ticket = Ticket::new(ticket.id, String::from(&ticket.author));    
+    let mut tickets = data.tickets.lock().unwrap();
+    tickets.push(new_ticket.clone());
+    new_ticket.respond_to(&req)
 }
 
 struct AppState{
-    tickets: Mutex<Vec<String>>,
+    tickets: Mutex<Vec<Ticket>>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize,Debug,Clone)]
 struct Ticket{
   id: u32,
   author: String,
 }
 
+impl Ticket{
+    fn new(id: u32, author: String) -> Self{ Ticket{ id, author } }
+}
+
 impl Responder for Ticket {
     type Body = BoxBody;
-
+    
     fn respond_to(self, _req: &HttpRequest) -> HttpResponse<Self::Body> {
         let res_body = serde_json::to_string(&self).unwrap();
         HttpResponse::Ok()
