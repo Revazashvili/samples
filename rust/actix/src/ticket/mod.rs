7f4c35b792242144523a1ed26d52d4dc6a483fb8
revazashvili <levanrevazashvili@gmail.com>
2022-08-20 18:12:46 +0400

    add header guard

--- rust/actix/src/ticket/mod.rs
+++ rust/actix/src/ticket/mod.rs
@@ -1,12 +1,13 @@
 pub mod models;
 pub mod handlers;
 
-use actix_web::web;
+use actix_web::{web,guard};
 use handlers::*;
 
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/api")
+        .guard(guard::Header("api-key", "actix"))
         .service(post_ticket)
         .service(get_tickets)
         .service(get_ticket)
