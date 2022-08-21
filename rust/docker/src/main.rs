b41c8f19af548b8e8a7bac1e244f000143e51d48
revazashvili <levanrevazashvili@gmail.com>
2022-08-21 12:39:09 +0400

    add /ping endpoint using actix

--- rust/docker/src/main.rs
+++ rust/docker/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use actix_web::{HttpServer, App, web};
+use actix_web::middleware::Logger;
+use env::set_var;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    set_var("RUST_BACKTRACE", "1");
+    set_var("RUST_LOG", "info");
+    env_logger::init();
+
+    HttpServer::new(move ||{
+        App::new().wrap(Logger::default()).route("/ping",web::get().to(|| async {"pong"}))
+        })
+        .bind("127.0.0.1:7878")?
+        .run()
+        .await
 }
