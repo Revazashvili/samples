6e5b51c32858d556c1adde0a02146744996139a1
revazashvili <levanrevazashvili@gmail.com>
2022-08-21 18:55:54 +0400

    create sum graphs in grafana

--- prometheus/dotnet/src/Controllers/WeatherForecastController.cs
+++ prometheus/dotnet/src/Controllers/WeatherForecastController.cs
@@ -1,4 +1,6 @@
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Prometheus;
 
 namespace dotnet.Controllers;
 
@@ -6,6 +8,7 @@ namespace dotnet.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
+    private static readonly Counter ProcessedJobCount = Metrics.CreateCounter("dotnet_request_operations_total", "The total number of processed requests");
     private static readonly string[] Summaries = new[]
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -21,12 +24,27 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        var sw = Stopwatch.StartNew();
+        var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+
+        sw.Stop();
+        
+        ProcessedJobCount.Inc();
+        var histogram = Metrics.CreateHistogram(
+                "dotnet_request_duration_seconds",
+                "Histogram for the duration in seconds.",
+                new HistogramConfiguration
+                {
+                    Buckets = Histogram.LinearBuckets(start: 1, width: 1, count: 5)
+                });
+
+        histogram.Observe(sw.Elapsed.TotalSeconds);
+        return result;
     }
 }
