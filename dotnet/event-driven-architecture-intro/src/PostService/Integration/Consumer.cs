feb54b1a96b40f84c38bd69ed41173cfbf49b9f5
revazashvili <levanrevazashvili@gmail.com>
2022-09-03 13:21:43 +0400

    remove some code

--- dotnet/event-driven-architecture-intro/src/PostService/Integration/Consumer.cs
+++ dotnet/event-driven-architecture-intro/src/PostService/Integration/Consumer.cs
@@ -47,31 +47,6 @@ public class Consumer : IConsumer
             autoAck: true,
             consumer: consumer);
     }
-    
-    public enum Direction
-    {
-        Up,
-        Down,
-        Right,
-        Left
-    }
-
-    public enum Orientation
-    {
-        North,
-        South,
-        East,
-        West
-    }
-
-    public static Orientation ToOrientation(Direction direction) => direction switch
-    {
-        Direction.Up    => Orientation.North,
-        Direction.Right => Orientation.East,
-        Direction.Down  => Orientation.South,
-        Direction.Left  => Orientation.West,
-        _ => throw new ArgumentOutOfRangeException(nameof(direction), $"Not expected direction value: {direction}"),
-    };
 
 
     private void UpdateUser(BasicDeliverEventArgs ea, PostServiceContext database)
