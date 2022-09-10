57ac739cc39798850484d2932f2100055ae9d9fc
revazashvili <levanrevazashvili@gmail.com>
2022-09-10 20:18:38 +0400

    add models module

---
+++ rust/warp/src/models.rs
@@ -0,0 +1,15 @@
+use serde::{Serialize,Deserialize};
+
+#[derive(Clone,Debug,Serialize,Deserialize)]
+pub struct Customer{
+    /// A unique identifier for this customer.
+    pub guid: String,
+    /// First name
+    pub first_name: String,
+    /// Last name
+    pub last_name: String,
+    /// Email address
+    pub email: String,
+    /// Physical Address
+    pub address: String,
+}
