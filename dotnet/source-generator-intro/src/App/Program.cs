1aaaa28dc3b1146110a1d2a70d6a380a6c901e75
revazashvili <levanrevazashvili@gmail.com>
2022-09-10 15:00:51 +0400

    generate some test class

--- dotnet/source-generator-intro/src/App/Program.cs
+++ dotnet/source-generator-intro/src/App/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using App;
+
+Test.P();
+
+
+public partial class Car
+{
+    [Give("Print")]
+    partial void Do();
+}
+
+public class Functions
+{
+    [Define]
+    public static void Print()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ dotnet/source-generator-intro/src/Generator/FunctionGenerator.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Generator;
+
+public class MainSyntaxReceiver : ISyntaxReceiver
+{
+    public int Index { get; set; }
+    public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+    {
+        if (syntaxNode is ClassDeclarationSyntax)
+        {
+            File.WriteAllText($@"/home/revazashvili/projects/samples/dotnet/source-generator-intro/src/Generator/{Index}.txt",syntaxNode.GetText().ToString());
+            Index++;
+        }
+    }
+}
+
+[Generator]
+public class FunctionGenerator : ISourceGenerator
+{
+    public void Execute(GeneratorExecutionContext context)
+    {
+        var output = @"
+public class Test
+{
+    public static void P() => Console.WriteLine(""Hello World!"");
+}
+";
+        context.AddSource("hello.g.cs", output);
+    }
+
+    public void Initialize(GeneratorInitializationContext context)
+    {
+        context.RegisterForSyntaxNotifications(() => new MainSyntaxReceiver());
+    }
+}
+
+
